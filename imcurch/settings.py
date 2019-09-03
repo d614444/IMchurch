63588ec05215f42d95fc2c43b8ec98e410dc1b3a
ronyaya <d614444@gmail.com>
2019-09-03 23:22:05 +0800

    imchurch_v7

--- imcurch/settings.py
+++ imcurch/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'king',
     'rest_framework',
     'phonenumber_field',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.twitter.TwitterOAuth',
 ]
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -134,8 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
 
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static','scss')
+print (SASS_PROCESSOR_ROOT)
--- static/script/navbarsmooth.js
+++ static/script/navbarsmooth.js
@@ -2,7 +2,7 @@ $(function(){
     $(' .nav-link').click(function() {
         var sectionTo = $(this).attr('href');
         $('html, body').animate({
-          scrollTop: ($(sectionTo).offset().top)-70
+          scrollTop: ($(sectionTo).offset().top)-100
         }, 1000);
     });
 })
